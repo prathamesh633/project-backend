da6e4bbf0554a622e2048388a5b30dd086810020
prathamesh633 <prathmeshbhujade1234@gmail.com>
2025-04-26 13:01:39 +0530

    added new stages

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,8 @@
 pipeline {
     agent any 
+    environment {
+        DOCKER_CREDENTIALS = credentials('docker-hub-creds')
+    }
     stages {
         stage('code-pull'){
             steps {
@@ -11,15 +14,30 @@ pipeline {
                sh 'mvn clean package'
             }
         }
-         stage('Deploy-K8s'){
+        stage('docker build'){
+            steps {
+               sh 'docker build . -t prathameshbhujade/project-backend-img:latest'
+            }
+        }    
+        stage('docker login') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+                    sh 'echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin'
+
+                }
+            }
+        }
+        stage('docker push'){
             steps {
                sh '''
-                    docker build . -t prathameshbhujade/project-backend-img:latest
-                    docker push prathameshbhujade/project-backend-img:latest
-                    docker rmi prathameshbhujade/project-backend-img:latest
-                    kubectl apply -f ./deploy/
-
-               '''
+                docker push prathameshbhujade/project-backend-img:latest
+                docker rmi prathameshbhujade/project-backend-img:latest
+                '''
+            }
+        }
+        stage('Deploy-K8s'){
+            steps {
+               sh 'kubectl apply -f ./k8s/'
             }
         }
     }
